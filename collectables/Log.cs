e42703c33b2bd02542184268b06783c9014d1d35
emilyrhee <emilyrhee99@gmail.com>
2024-05-16 17:13:06 -0400

    fixed log collection

--- collectables/Log.cs
+++ collectables/Log.cs
@@ -4,18 +4,17 @@ using System;
 public partial class Log : AnimatedSprite2D
 {
     private RichTextLabel logLabel;
-    private int logCount = 0;
+
 	public override void _Ready() {
-		logLabel = GetNode<RichTextLabel>("../CanvasLayer/Logcount/Count");
-        logLabel.AddText(logCount.ToString());
+        logLabel = GetNode<RichTextLabel>("../CanvasLayer/Logcount/Count");
 	}
 
 	public void _on_log_area_body_entered(Node2D body) {
 		if (body.Name == "Player")
 		{
-			logCount++;
+            Global.logsCollected++;
             logLabel.Clear();
-            logLabel.AddText(logCount.ToString());
+            logLabel.AddText(Global.logsCollected.ToString());  
 			QueueFree();
 		}
 	}
--- levels/Level1.cs
+++ levels/Level1.cs
@@ -3,11 +3,15 @@ using System;
 
 public partial class Level1 : Node2D
 {
+    private RichTextLabel logLabel;
 	private AudioStreamPlayer2D music;
 	public override void _Ready()
 	{
 		music = GetNode<AudioStreamPlayer2D>("BackgroundMusic");
 		music.Playing = true;
+        
+		logLabel = GetNode<RichTextLabel>("CanvasLayer/Logcount/Count");
+        logLabel.AddText("0");
 	}
 	public void _on_background_music_finished()
 	{
