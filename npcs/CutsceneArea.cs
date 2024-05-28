b8d5bf1be36205b11345cc726dc12bb8ecffbc01
emilyrhee <emilyrhee99@gmail.com>
2024-05-27 21:22:33 -0400

    added moar txt on key press

--- npcs/CutsceneArea.cs
+++ npcs/CutsceneArea.cs
@@ -4,6 +4,7 @@ using System;
 public partial class CutsceneArea : Area2D
 {
     private RichTextLabel dialog;
+    private bool playerIsNearby = false;
     [Export] Font font;
 	public override void _Ready()
 	{
@@ -11,15 +12,35 @@ public partial class CutsceneArea : Area2D
         dialog.PushFont(font);
         dialog.PushFontSize(11);
 	}
+    private void ResetTextTo(string text)
+    {
+        dialog.Clear();
+        dialog.PushFont(font);
+        dialog.PushFontSize(11);
+        dialog.AddText(text);
+    }
     public void _on_body_entered(Node2D body)
     {
         if (body.Name == "Player")
         {
-            dialog.AddText("Hello there!");
-            GD.Print("hello there!");
+            dialog.AddText("Hello! ▶");
+            playerIsNearby = true;
         }
     }
-	public override void _Process(double delta)
-	{
-	}
+    public void _on_body_exited(Node2D body)
+    {
+        playerIsNearby = false;
+        Player.jumpVelocity = -250.0f;
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if (playerIsNearby && @event is InputEventKey eventKey && eventKey.Pressed)
+        {
+            if (Input.IsActionJustPressed("ui_accept"))
+            {
+                Player.jumpVelocity = 0;
+                ResetTextTo("How are you?");
+            }
+        }
+    }
 }
