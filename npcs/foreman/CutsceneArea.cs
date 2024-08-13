461ce901ce863f19f50f217638b9377b6fe61bda
emilyrhee <emilyrhee99@gmail.com>
2024-08-12 22:16:05 -0400

    added prompt to proceed with dialog

--- npcs/foreman/CutsceneArea.cs
+++ npcs/foreman/CutsceneArea.cs
@@ -26,7 +26,7 @@ public partial class CutsceneArea : Area2D
     {
         if (body is Player) // argument MUST be a Node2D, hence the node type MUST be checked
         {
-            dialog.AddText("Hello! How are you?");
+            dialog.AddText("Hello! How are you? (space to continue)");
 
             Player.speed = 0f;
 
