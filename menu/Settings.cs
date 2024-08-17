f1130fea471526ce6070403e0cdda575348f8a36
emilyrhee <emilyrhee99@gmail.com>
2024-08-16 22:16:36 -0400

    exit menu if ui_cancel pressed again

--- menu/Settings.cs
+++ menu/Settings.cs
@@ -3,14 +3,20 @@ using System;
 
 public partial class Settings : Node2D
 {
+    public override void _Ready()
+    {
+        Visible = false;
+    }
+
     public override void _Input(InputEvent @event)
     {
         if (@event.IsActionPressed("ui_cancel"))
         {
-            Visible = true;
+            Visible = !Visible;
         }
     }
-	public override void _Process(double delta)
-	{
-	}
+
+    public override void _Process(double delta)
+    {
+    }
 }
